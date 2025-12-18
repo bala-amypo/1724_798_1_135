bb3fc3c153e0c5b46519e997f9777a75ed3ccc08
bala-amypo <bala@amypo.in>
2025-12-18 05:57:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.eventsystem.controller;
+
+import com.example.eventsystem.entity.User;
+import com.example.eventsystem.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+@Tag(name = "Users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // POST /register
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return new ResponseEntity<>(userService.register(user), HttpStatus.CREATED);
+    }
+
+    // GET /{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    // GET /
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAll());
+    }
+}
