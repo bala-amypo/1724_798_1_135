5c684cf181dd068c0a8bf1da5a8ff8e097e86088
bala-amypo <bala@amypo.in>
2025-12-22 09:47:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/SubscriptionRepository.java
+++ src/main/java/com/example/demo/repository/SubscriptionRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
     boolean existsByUserIdAndEventId(Long userId, Long eventId);
     List<Subscription> findByUserId(Long userId);
+    List<Subscription> findByEventId(Long eventId);
 }
