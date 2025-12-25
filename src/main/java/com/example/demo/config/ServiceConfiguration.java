fbfd254aca78077b6823052c7329eb2f0fbc62c7
bala-amypo <bala@amypo.in>
2025-12-25 09:32:55 +0000

    User commit

--- src/main/java/com/example/demo/config/ServiceConfiguration.java
+++ src/main/java/com/example/demo/config/ServiceConfiguration.java
@@ -3,9 +3,8 @@ package com.example.demo.config;
 import com.example.demo.service.BroadcastService;
 import com.example.demo.service.EventUpdateService;
 import com.example.demo.service.impl.EventUpdateServiceImpl;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-import javax.annotation.PostConstruct;
 
 @Configuration
 public class ServiceConfiguration {
