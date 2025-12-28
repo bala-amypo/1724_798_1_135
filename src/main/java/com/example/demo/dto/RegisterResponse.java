d2d43af2973404157b34a7176ce22eed7853089a
bala-amypo <bala@amypo.in>
2025-12-28 07:47:40 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterResponse.java
+++ src/main/java/com/example/demo/dto/RegisterResponse.java
@@ -1,20 +1,29 @@
 package com.example.demo.dto;
+import com.example.demo.User;
+
 
 public class RegisterResponse {
     private String fullName;
     private String email;
     private String password;
     private String role;
+    private Long id;
+    User user = new User();
 
     
-   public RegisterResponse(String fullName , String email, String role)
+   public RegisterResponse(Long id , String fullName , String email, String role)
    {
+       this.id = user.get
        this.fullName = fullName;
        this.email = email;
        this.role = role;
 
    }
-
+     
+    public String getId()
+    {
+        return id;
+    } 
     
     public String getFullName() {
         return fullName;
