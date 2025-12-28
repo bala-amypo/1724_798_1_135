daad10e18573809202d329f37f331f6507e435b1
bala-amypo <bala@amypo.in>
2025-12-28 07:20:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/RegisterResponse.java
@@ -0,0 +1,35 @@
+package com.example.demo.dto;
+
+public class RegisterResponse {
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+
+    
+   public RegisterR
+
+    
+    public String getFullName() {
+        return fullName;
+    }
+    
+   
+    
+    public String getEmail() {
+        return email;
+    }
+    
+  
+    
+    public String getPassword() {
+        return password;
+    }
+    
+
+    public String getRole() {
+        return role;
+    }
+    
+   
+}
